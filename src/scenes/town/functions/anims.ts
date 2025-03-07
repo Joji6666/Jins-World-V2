f35387104a2acf92fa9a57ea53ac95121cc7ba10
KimJin <joji6666@github.com>
2025-03-07 09:32:18 +0900

    몬스터 로드방식 변경

--- src/scenes/town/functions/anims.ts
+++ src/scenes/town/functions/anims.ts
@@ -1,54 +1,297 @@
-import { SIDES } from "../../../shared/constants/keys";
-
 export const createOrcAnims = (
   scene: Phaser.Scene,
   numbering: number
 ): void => {
   const animationConfig = [
     {
-      name: "idle",
-      startFrame: 0,
-      endFrame: 3
+      key: `orc_${numbering}_idle_front`,
+      start: 0,
+      end: 3,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_idle`
     },
     {
-      name: "attack",
-      startFrame: 0,
-      endFrame: 7
+      key: `orc_${numbering}_idle_back`,
+      start: 4,
+      end: 7,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_idle`
     },
     {
-      name: "death",
-      startFrame: 0,
-      endFrame: 7
+      key: `orc_${numbering}_idle_right`,
+      start: 8,
+      end: 11,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_idle`
     },
     {
-      name: "hurt",
-      startFrame: 0,
-      endFrame: 5
+      key: `orc_${numbering}_idle_left`,
+      start: 12,
+      end: 15,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_idle`
+    },
+
+    {
+      key: `orc_${numbering}_attack_front`,
+      start: 0,
+      end: 7,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_attack`
     },
     {
-      name: "walk",
-      startFrame: 0,
-      endFrame: 5
+      key: `orc_${numbering}_attack_back`,
+      start: 8,
+      end: 15,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_attack`
+    },
+    {
+      key: `orc_${numbering}_attack_right`,
+      start: 16,
+      end: 23,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_attack`
+    },
+    {
+      key: `orc_${numbering}_attack_left`,
+      start: 24,
+      end: 31,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_attack`
+    },
+
+    {
+      key: `orc_${numbering}_death_front`,
+      start: 0,
+      end: 7,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_death`
+    },
+    {
+      key: `orc_${numbering}_death_back`,
+      start: 8,
+      end: 15,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_death`
+    },
+    {
+      key: `orc_${numbering}_death_right`,
+      start: 16,
+      end: 23,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_death`
+    },
+    {
+      key: `orc_${numbering}_death_left`,
+      start: 24,
+      end: 31,
+      frameRate: 10,
+      repeat: 0,
+      sourceKey: `orc_${numbering}_death`
     }
   ];
 
-  animationConfig.forEach((config) => {
-    SIDES.forEach((side) => {
-      scene.anims.create({
-        key: `orc_${numbering}_${config.name}_${side}`,
-
-        frames: scene.anims.generateFrameNumbers(
-          `orc_${numbering}_${config.name}_${side}`,
-          {
-            start: config.startFrame,
-            end: config.endFrame
-          }
-        ),
-
+  if (numbering < 3) {
+    animationConfig.push(
+      {
+        key: `orc_${numbering}_walk_front`,
+        start: 0,
+        end: 5,
         frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_walk`
+      },
+      {
+        key: `orc_${numbering}_walk_back`,
+        start: 6,
+        end: 11,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_walk`
+      },
+      {
+        key: `orc_${numbering}_walk_right`,
+        start: 12,
+        end: 17,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_walk`
+      },
+      {
+        key: `orc_${numbering}_walk_left`,
+        start: 18,
+        end: 23,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_walk`
+      }
+    );
 
-        repeat: -1
+    if (numbering === 2) {
+      animationConfig.push(
+        {
+          key: `orc_${numbering}_walk_attack_front`,
+          start: 0,
+          end: 5,
+          frameRate: 10,
+          repeat: 0,
+          sourceKey: `orc_${numbering}_walk_attack`
+        },
+        {
+          key: `orc_${numbering}_walk_attack_back`,
+          start: 6,
+          end: 11,
+          frameRate: 10,
+          repeat: 0,
+          sourceKey: `orc_${numbering}_walk_attack`
+        },
+        {
+          key: `orc_${numbering}_walk_attack_right`,
+          start: 12,
+          end: 17,
+          frameRate: 10,
+          repeat: 0,
+          sourceKey: `orc_${numbering}_walk_attack`
+        },
+        {
+          key: `orc_${numbering}_walk_attack_left`,
+          start: 18,
+          end: 23,
+          frameRate: 10,
+          repeat: 0,
+          sourceKey: `orc_${numbering}_walk_attack`
+        }
+      );
+    }
+  } else {
+    animationConfig.push(
+      {
+        key: `orc_${numbering}_run_front`,
+        start: 0,
+        end: 7,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run`
+      },
+      {
+        key: `orc_${numbering}_run_back`,
+        start: 8,
+        end: 15,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run`
+      },
+      {
+        key: `orc_${numbering}_run_right`,
+        start: 16,
+        end: 23,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run`
+      },
+      {
+        key: `orc_${numbering}_run_left`,
+        start: 24,
+        end: 31,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run`
+      },
+      {
+        key: `orc_${numbering}_run_attack_front`,
+        start: 0,
+        end: 7,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run_attack`
+      },
+      {
+        key: `orc_${numbering}_run_attack_back`,
+        start: 8,
+        end: 15,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run_attack`
+      },
+      {
+        key: `orc_${numbering}_run_attack_right`,
+        start: 16,
+        end: 23,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run_attack`
+      },
+      {
+        key: `orc_${numbering}_run_attack_left`,
+        start: 24,
+        end: 31,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_run_attack`
+      }
+    );
+  }
+
+  if (numbering > 1) {
+    animationConfig.push(
+      {
+        key: `orc_${numbering}_hurt_front`,
+        start: 0,
+        end: 5,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_hurt`
+      },
+      {
+        key: `orc_${numbering}_hurt_back`,
+        start: 6,
+        end: 11,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_hurt`
+      },
+      {
+        key: `orc_${numbering}_hurt_right`,
+        start: 12,
+        end: 17,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_hurt`
+      },
+      {
+        key: `orc_${numbering}_hurt_left`,
+        start: 18,
+        end: 23,
+        frameRate: 10,
+        repeat: 0,
+        sourceKey: `orc_${numbering}_hurt`
+      }
+    );
+  }
+
+  animationConfig.forEach(
+    ({ key, start, end, frameRate, repeat, sourceKey }) => {
+      scene.anims.create({
+        key: `${key}`,
+        frames: scene.anims.generateFrameNumbers(sourceKey, {
+          start,
+          end
+        }),
+        frameRate,
+        repeat
       });
-    });
-  });
+    }
+  );
 };
--- src/scenes/town/functions/preload.ts
+++ src/scenes/town/functions/preload.ts
@@ -1,15 +1,60 @@
-import { SIDES } from "../../../shared/constants/keys";
-
 export const townPreload = (scene: Phaser.Scene): void => {
   scene.load.tilemapTiledJSON("town-map", "/assets/map2.json");
   scene.load.image("town-tiles", "/assets/map2.png");
 };
 
 export const orcPreload = (scene: Phaser.Scene, numbering: number): void => {
-  SIDES.forEach((side) => {
+  scene.load.spritesheet(
+    `orc_${numbering}_idle`,
+    `/assets/monsters/orc_${numbering}/orc_${numbering}_idle.png`,
+    {
+      frameWidth: 64,
+      frameHeight: 64
+    }
+  );
+
+  scene.load.spritesheet(
+    `orc_${numbering}_attack`,
+    `/assets/monsters/orc_${numbering}/orc_${numbering}_attack.png`,
+    {
+      frameWidth: 64,
+      frameHeight: 64
+    }
+  );
+
+  scene.load.spritesheet(
+    `orc_${numbering}_death`,
+    `/assets/monsters/orc_${numbering}/orc_${numbering}_death.png`,
+    {
+      frameWidth: 64,
+      frameHeight: 64
+    }
+  );
+
+  if (numbering < 3) {
     scene.load.spritesheet(
-      `orc_${numbering}_idle_${side}`,
-      `/assets/monsters/orc${numbering}/orc_${numbering}_idle_${side}.png`,
+      `orc_${numbering}_walk`,
+      `/assets/monsters/orc_${numbering}/orc_${numbering}_walk.png`,
+      {
+        frameWidth: 64,
+        frameHeight: 64
+      }
+    );
+
+    if (numbering > 1) {
+      scene.load.spritesheet(
+        `orc_${numbering}_walk_attack`,
+        `/assets/monsters/orc_${numbering}/orc_${numbering}_walk_attack.png`,
+        {
+          frameWidth: 64,
+          frameHeight: 64
+        }
+      );
+    }
+  } else {
+    scene.load.spritesheet(
+      `orc_${numbering}_run`,
+      `/assets/monsters/orc_${numbering}/orc_${numbering}_run.png`,
       {
         frameWidth: 64,
         frameHeight: 64
@@ -17,39 +62,23 @@ export const orcPreload = (scene: Phaser.Scene, numbering: number): void => {
     );
 
     scene.load.spritesheet(
-      `orc_${numbering}_death_${side}`,
-      `/assets/monsters/orc${numbering}/orc_${numbering}_death_${side}.png`,
+      `orc_${numbering}_run_attack`,
+      `/assets/monsters/orc_${numbering}/orc_${numbering}_run_attack.png`,
       {
         frameWidth: 64,
         frameHeight: 64
       }
     );
+  }
 
+  if (numbering > 1) {
     scene.load.spritesheet(
-      `orc_${numbering}_hurt_${side}`,
-      `/assets/monsters/orc${numbering}/orc_${numbering}_hurt_${side}.png`,
+      `orc_${numbering}_hurt`,
+      `/assets/monsters/orc_${numbering}/orc_${numbering}_hurt.png`,
       {
         frameWidth: 64,
         frameHeight: 64
       }
     );
-
-    scene.load.spritesheet(
-      `orc_${numbering}_walk_${side}`,
-      `/assets/monsters/orc${numbering}/orc_${numbering}_walk_${side}.png`,
-      {
-        frameWidth: 64,
-        frameHeight: 64
-      }
-    );
-
-    scene.load.spritesheet(
-      `orc_${numbering}_attack_${side}`,
-      `/assets/monsters/orc${numbering}/orc_${numbering}_attack_${side}.png`,
-      {
-        frameWidth: 64,
-        frameHeight: 64
-      }
-    );
-  });
+  }
 };
